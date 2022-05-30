b3696f32f7c35e8486555829ed362a4380b7d310
Сашенька <a2003a.k01@gmail.com>
2022-05-30 15:28:33 +0500

    Добавьте файлы проекта.

---
+++ Chernovic_01/Pages/materialPage.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chernovic_01.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для materialPage.xaml
+    /// </summary>
+    public partial class materialPage : Page
+    {
+        List<Materials> listMaterialN;
+
+        private int tekPage = 1;
+        private int countMat = 15;
+        private int maxPage;
+        private int vsegoMat;
+
+        public materialPage()
+        {
+            InitializeComponent();
+            listMaterialN = ChernovicEntities.GetContext().Materials.ToList();
+            List<TypeMaterial> spisokMaterial = ChernovicEntities.GetContext().TypeMaterial.ToList();
+            spisokMaterial.Insert(0, new TypeMaterial { NameType = "Все типы" });
+            cbFilter.ItemsSource = spisokMaterial;
+            cbFilter.SelectedIndex = 0;
+            vsegoMat = listMaterialN.Count;
+            Refresh();
+        }
+
+        public void Refresh()
+        {
+            maxPage = (int)Math.Ceiling((listMaterialN.Count * 1.0) / countMat);
+            var listMaterialPage = listMaterialN.Skip((tekPage - 1) * countMat).Take(countMat).ToList();
+            tbTekPage.Text = tekPage.ToString();
+            lbVsegoPage.Content = "из " + maxPage.ToString();
+            //tbSkolcoDanih.Text = "Показано: " + listMaterialN.Count + " из " + listMaterialN.Count;
+            listMaterial.ItemsSource = listMaterialPage;
+        }
+        public void UpdateList()
+        {
+            listMaterialN = null;
+            listMaterialN = ChernovicEntities.GetContext().Materials.ToList();
+            Refresh();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            cbSort.ItemsSource = new List<string> { "По возрастанию наименования", "По убыванию ниманования","По возрастанию остаток на складе", "По убыванию остаток на складе", "По возрастанию стоимость", "По убыванию стоимость"};
+        }
+
+        private void tbPoisk_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            listMaterialN = listMaterialN.Where(p => p.NameMaterial.ToLower().Contains(tbPoisk.Text.ToLower())).ToList();
+            if(String.IsNullOrWhiteSpace(tbPoisk.Text))
+            {
+                listMaterialN = ChernovicEntities.GetContext().Materials.ToList();
+            }
+  
+            Refresh();
+            tbSkolcoDanih.Text = "Показано: " + listMaterialN.Count + " из " + vsegoMat.ToString();
+        }
+
+        private void cbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (cbSort.SelectedIndex)
+            {
+                case 0:
+                    listMaterialN = listMaterialN.OrderBy(p => p.NameMaterial).ToList();
+                    break;
+                case 2:
+                    listMaterialN = listMaterialN.OrderBy(p => p.KolNaSklade).ToList();
+                    break;
+                case 4:
+                    listMaterialN = listMaterialN.OrderBy(p => p.Cena).ToList();
+                    break;
+                case 1:
+                    listMaterialN = listMaterialN.OrderByDescending(p => p.NameMaterial).ToList();
+                    break;
+                case 3:
+                    listMaterialN = listMaterialN.OrderByDescending(p => p.KolNaSklade).ToList();
+                    break;
+                case 5:
+                    listMaterialN = listMaterialN.OrderByDescending(p => p.Cena).ToList();
+                    break;
+            }
+            Refresh();
+        }
+
+        private void cbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            listMaterialN = ChernovicEntities.GetContext().Materials.ToList();
+            listMaterialN = listMaterialN.Where(p => p.IdType == (cbFilter.SelectedItem as TypeMaterial).Id).ToList();
+            if(cbFilter.SelectedIndex == 0)
+            {
+                listMaterialN = ChernovicEntities.GetContext().Materials.ToList();
+            }
+           
+            Refresh();
+            tbSkolcoDanih.Text = "Показано: " + listMaterialN.Count + " из " + vsegoMat.ToString();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Materials newMater = new Materials();
+            Windows.addEditMaterial addEditMaterial = new Windows.addEditMaterial(newMater);
+            addEditMaterial.ShowDialog();
+            UpdateList();
+            /* или
+           Windows.addEditMaterial addEditMaterial = new Windows.addEditMaterial(null);
+            addEditMaterial.ShowDialog();
+            UpdateList();
+             */
+        }
+
+        private void btnVNachalo_Click(object sender, RoutedEventArgs e)
+        {
+            tekPage = 1;
+            Refresh();
+        }
+
+        private void btnNazadS_Click(object sender, RoutedEventArgs e)
+        {
+            if (tekPage <= 1)
+                tekPage = 1;
+            else
+                tekPage--;
+            Refresh();
+        }
+
+        private void btnVpered_Click(object sender, RoutedEventArgs e)
+        {
+            if (tekPage >= maxPage)
+                tekPage = maxPage;
+            else
+                tekPage++;
+            Refresh();
+        }
+
+        private void btnVKonez_Click(object sender, RoutedEventArgs e)
+        {
+            tekPage = maxPage;
+            Refresh();
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            
+                Windows.addEditMaterial addEditMaterial = new Windows.addEditMaterial((sender as Button).DataContext as Materials);//или listMaterial.SelectedItem as Materials
+                addEditMaterial.ShowDialog();
+                UpdateList();
+            
+        }
+
+        private void btnNazad_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
---
+++ Chernovic_01/Windows/addEditMaterial.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chernovic_01.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для addEditMaterial.xaml
+    /// </summary>
+    public partial class addEditMaterial : Window
+    {
+        Materials _materials = new Materials();
+
+        public addEditMaterial(Materials materials)
+        {
+            InitializeComponent();
+            _materials = materials;
+            if(_materials.Id !=0) //или _materials != null
+            {
+                listPostavok.ItemsSource = ChernovicEntities.GetContext().MaretialPostvshik.Where(p => p.idMaterial == _materials.Id).ToList();
+                cbType.ItemsSource = ChernovicEntities.GetContext().TypeMaterial.Where(p => p.Id == _materials.IdType).ToList();
+                cbType.SelectedIndex = 0;
+            }
+            cbType.ItemsSource = ChernovicEntities.GetContext().TypeMaterial.ToList();
+            cbPost.ItemsSource = ChernovicEntities.GetContext().Postavshiki.ToList();
+            DataContext = _materials;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (int.Parse(tbMin.Text) <= 0)
+                {
+                    MessageBox.Show("Минимальное количество не может быть отрицательным");
+                }
+                else
+                {
+                    if (_materials.Id == 0) //или _materials == null
+                    {
+
+
+                        _materials.IdType = cbType.SelectedIndex + 1;
+                        ChernovicEntities.GetContext().Materials.Add(_materials);
+
+
+                    }
+                    ChernovicEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Сохранено");
+                    this.Close();
+                }
+          
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if(MessageBox.Show("Вы действительно хотите удалить запись?","Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                   List<MaretialPostvshik> maretialPostvshiks = ChernovicEntities.GetContext().MaretialPostvshik.Where(p => p.idMaterial == _materials.Id).ToList();
+                   if(maretialPostvshiks.Count>0)
+                    {
+                        foreach (MaretialPostvshik mP in maretialPostvshiks)
+                        {
+                            ChernovicEntities.GetContext().MaretialPostvshik.Remove(mP);
+                         
+                        }
+                        ChernovicEntities.GetContext().Materials.Remove(_materials);
+                        ChernovicEntities.GetContext().SaveChanges();
+                        MessageBox.Show("Запись удалена");
+                        this.Close();
+                    }
+                    else
+                    {
+                        ChernovicEntities.GetContext().Materials.Remove(_materials);
+                        ChernovicEntities.GetContext().SaveChanges();
+                        MessageBox.Show("Запись удалена");
+                        this.Close();
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void btnAddP_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bool b = false;
+                List<MaretialPostvshik> spisoMatPost = ChernovicEntities.GetContext().MaretialPostvshik.Where(p => p.idMaterial == _materials.Id).ToList();
+                foreach(MaretialPostvshik mP in spisoMatPost)
+                {
+                    if(mP.idPostvshik == (cbPost.SelectedItem as Postavshiki).Id)
+                    {
+                        b = true;
+                    }
+                }
+                if(b == false)
+                {
+                    MaretialPostvshik newMaretialPostvshik = new MaretialPostvshik()
+                    {
+                        idMaterial = _materials.Id,
+                        idPostvshik = cbPost.SelectedIndex + 1
+                    };
+                    ChernovicEntities.GetContext().MaretialPostvshik.Add(newMaretialPostvshik);
+                    ChernovicEntities.GetContext().SaveChanges();
+                    listPostavok.ItemsSource = null;
+                    listPostavok.ItemsSource = ChernovicEntities.GetContext().MaretialPostvshik.Where(p => p.idMaterial == _materials.Id).ToList();
+                }
+                else
+                {
+                    MessageBox.Show("Такой постащик уже есть");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+          
+        }
+
+        private void btnOtmena_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tbPoisk_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            cbPost.ItemsSource = ChernovicEntities.GetContext().Postavshiki.Where(p => p.NamePostavshik.ToLower().Contains(tbPoisk.Text.ToLower())).ToList();
+            if(String.IsNullOrWhiteSpace(tbPoisk.Text))
+            {
+                cbPost.ItemsSource = ChernovicEntities.GetContext().Postavshiki.ToList();
+            }
+        }
+
+        private void btnDeletePos_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                        ChernovicEntities.GetContext().MaretialPostvshik.Remove((sender as Button).DataContext as MaretialPostvshik);
+                        ChernovicEntities.GetContext().SaveChanges();
+                        MessageBox.Show("Запись удалена");
+                       
+                  
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
