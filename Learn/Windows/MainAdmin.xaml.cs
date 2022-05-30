103e398810bacb19e6011d7cb280fcdee06afbc1
Сашенька <a2003a.k01@gmail.com>
2022-05-30 15:11:12 +0500

    Добавьте файлы проекта.

---
+++ Learn/Windows/MainAdmin.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Learn.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Learn.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainAdmin.xaml
+    /// </summary>
+    public partial class MainAdmin : Window
+    {
+        public MainAdmin(string code)
+        {
+            InitializeComponent();
+            MangerFrame.MainFrame = MainFrame;
+            Classes.CodeAdmin cA = new Classes.CodeAdmin();
+            if (code != "0000")
+            {
+                MainFrame.Navigate(new Pages.PageServise(code));
+            }
+            else
+            {
+                MainFrame.Navigate(new Pages.MainPage());
+            }
+        }
+    }
+}
